4b6078ff7eda39eeed1382316f7bc8fb2ef125d1
MarkFormigones <markformigones@gmail.com>
2023-07-02 00:09:30 +0800

    Portfolio

---
+++ Views/Pages/Download/DownloadController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Starterkit.Views.Pages.Download
+{
+    public class DownloadController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult DownloadCV()
+        {
+            string filePath = "wwwroot/assets/media/pdf/mark-anthony-formigones.pdf";
+            string fileName = "Mark Anthony Formigones CV";
+
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+
+            return File(fileBytes, "application/pdf", fileName);
+        }
+    }
+}
+
---
+++ Views/Pages/Profile/ProfileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Starterkit._keenthemes.libs;
+
+namespace Starterkit.Controllers;
+
+public class ProfileController : Controller
+{
+    private readonly ILogger<ProfileController> _logger;
+    private readonly IKTTheme _theme;
+
+    public ProfileController(ILogger<ProfileController> logger, IKTTheme theme)
+    {
+        _logger = logger;
+        _theme = theme;
+    }
+
+    [HttpGet("/")]
+    [HttpGet("/Profile")]
+    public IActionResult Index()
+    {
+        return View(_theme.GetPageView("Profile", "Index.cshtml"));
+    }
+}
